8e1fb73f74fd7c297aba8cf7e8a76b2a356f39ae
Erik-Texher <16151233@aguascalientes.tecnm.mx>
2022-05-10 00:07:26 -0500

    iconos y color al tab
    Se le agrego iconos y color a la barra de navegación.

--- invernatron/navigations/Navigation.js
+++ invernatron/navigations/Navigation.js
@@ -4,23 +4,55 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 import Invernadero from '../screens/Invernadero'
 import Configuracion from '../screens/Configuracion'
+import { Icon } from '@rneui/base'
+
 
 
 const Tab = createBottomTabNavigator()
 
 export default function Navigation() {
-  return (
-    <NavigationContainer>
-        <Tab.Navigator>
-            <Tab.Screen
-            name="Invernadero"
-            component={Invernadero}
+    const screenOptions = (route, color) => {
+        let iconName
+        switch (route.name) {
+            case "Invernadero":
+                iconName = "nature"
+                break;
+            case "Configuracion":
+                iconName = "format-list-bulleted"
+                break;
+            default:
+                break;
+        }
+        return(
+            <Icon
+            type="material-comunity"
+            name={iconName}
+            size={22}
+            color={color}
             />
-            <Tab.Screen
-            name="Configuración"
-            component={Configuracion}
-            />
-        </Tab.Navigator>
-    </NavigationContainer>
-  )
+        )
+    }
+    return (
+        <NavigationContainer>
+            <Tab.Navigator
+            initialRouteName='Invernadero'
+            tabBarOptions={{
+                inactiveTintColor:"#73b767",
+                activeTintColor:"#3e6339"
+            }}
+            screenOptions={({ route }) =>({
+                tabBarIcon: ({color}) => screenOptions(route, color)
+            })}
+            >
+                <Tab.Screen
+                    name="Invernadero"
+                    component={Invernadero}
+                />
+                <Tab.Screen
+                    name="Configuracion"
+                    component={Configuracion}
+                />
+            </Tab.Navigator>
+        </NavigationContainer>
+    )
 }

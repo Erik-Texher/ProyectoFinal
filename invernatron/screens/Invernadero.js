e3633b44fb5396689a17d28366118f6119b18494
Erik-Texher <16151233@aguascalientes.tecnm.mx>
2022-05-10 20:13:43 -0500

    Imagen
    Se agrega una imagen principal y se comienza a hacer la interfaz

--- invernatron/screens/Invernadero.js
+++ invernatron/screens/Invernadero.js
@@ -1,12 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native'
 import React from 'react'
 
 export default function Invernadero() {
   return (
-    <View>
-      <Text>Invernadero</Text>
-    </View>
+    <ScrollView
+      centerContent
+      style={styles.viewBody}
+    >
+      <Image
+        source={require("../../invernatron/assets/Logo_inv.png")}
+        resizeMode="contain"
+        style={styles.image}
+      />
+    </ScrollView>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  viewBody: {
+    marginHorizontal: 30
+  },
+  image: {
+    height: 300,
+    width: "100%",
+    marginBottom: 10,
+    textAlign: "center"
+
+  }
+})
